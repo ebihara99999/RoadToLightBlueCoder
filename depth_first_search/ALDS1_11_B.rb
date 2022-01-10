9130c79bbe03d26d474d638b4909f4962f10b8d6
Yusuke Ebihara <dev.biibiebi@gmail.com>
2022-01-11 01:06:53 +0900

    wip

--- depth_first_search/ALDS1_11_B.rb
+++ depth_first_search/ALDS1_11_B.rb
@@ -1,54 +1,61 @@
 # frozen_string_literal: true
 
 class Node
-   def initialize(val, *nodes)
-     @val = val 
-     @adjacents = nodes
-     @visited = false
-   end
+  attr_accessor :seen_at, :visited_at
+  attr_reader :val, :adjacent_ids
 
-   def visit
-     # do something
-   end
+  @@time = 0
+  @@nodes = []
 
-   def adjacent
-     @nodes
-   end
+  def initialize(val, *adjacent_ids)
+    @val = val
+    @adjacent_ids = adjacent_ids
+    @seen_at = 0
+    @visited_at = 0
+  end
 
-   def visited!
-     @visited = true
-   end
+  def visited?
+    !visited_at.zero?
+  end
 
-   def visited?
-     @visited
-   end
-end
+  def seen?
+    !seen_at.zero?
+  end
 
-def dfs(node)
-  node.visit
-  node.visited!
+  def self.dfs(node)
+    @@time += 1
+    node.visited_at = @@time
 
-  node.adjacents.each do |child|
-    unless child.visited?
-      dfs child
+    node.adjacent_ids.each do |id|
+      child = @@nodes.select { |adj| adj.val == id }.first
+      child.seen_at = @@time unless child.seen?
+      dfs(child) unless child.visited?
     end
+  end
+
+  def self.nodes
+    @@nodes
+  end
+
+  def self.add_nodes(node)
+    @@nodes << node
   end
 end
 
 n = gets.chomp.to_i
-nodes = []
-binding.irb
 n.times do
   tmp = gets.chomp.split.map(&:to_i)
-  val = tmp.shift 
-  degree = tmp.shift
-  adjacents = tmp
+  val = tmp.shift
+  _ = tmp.shift
+  adjacent_ids = tmp
 
-  nodes << Node.new(val, *adjacents)
+  Node.add_nodes Node.new(val, *adjacent_ids)
 end
 
-binding.irb
+Node.nodes.each do |node|
+  Node.dfs(node)
+end
 
-nodes.each do |node|
-  
+Node.nodes.each do |node|
+  puts "#{node.val} #{node.seen_at} #{node.visited_at}"
 end
