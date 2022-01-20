b84124cdf9f1d5cbb1973e283d090fb1aed75371
Yusuke Ebihara <dev.biibiebi@gmail.com>
2022-01-20 23:26:34 +0900

    Passed:tada:
    ref: https://atcoder.jp/contests/abc138/submissions/28651532
    Thanks to https://drken1215.hatenablog.com/entry/2020/05/18/174000

--- depth_first_search/abc138_d.rb
+++ depth_first_search/abc138_d.rb
@@ -1,25 +1,30 @@
 # frozen_string_literal: true
 # ref: https://atcoder.jp/contests/abc138/tasks/abc138_d
 
-def dfs(graph, edge, parent, counter)
-  @counters[edge] += counter
-  graph[edge].each do |e|
-    next if e == parent
+def dfs(graph, v, pv)
+binding.irb
+  @counters[v] += @counters[pv] unless pv == -1
 
-    dfs(graph, e - 1, parent, counter)
+  graph[v].each do |e|
+    next if e == pv
+
+    dfs(graph, e, v)
   end
 end
 n, q = gets.chomp.split.map(&:to_i)
 graph = Array.new(n) { [] }
 (n - 1).times do
   a, b = gets.chomp.split.map(&:to_i)
-  graph[a - 1] << b
+  graph[a - 1] << b - 1
+  graph[b - 1] << a - 1
 end
 
 @counters = Array.new(n, 0)
 q.times do
-  edge, pj = gets.chomp.split.map(&:to_i)
-  dfs(graph, edge - 1, edge - 2, pj)
+  v, x = gets.chomp.split.map(&:to_i)
+  v -= 1
+  @counters[v] += x
 end
 
+dfs(graph, 0, -1)
 puts @counters.join(' ')
