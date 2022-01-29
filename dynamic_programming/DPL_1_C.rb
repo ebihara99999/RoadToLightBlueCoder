0c56983d1113424a05c85968e0f4add52ee669d4
Yusuke Ebihara <dev.biibiebi@gmail.com>
2022-01-29 23:33:31 +0900

    Passed:tada:
    ref: https://judge.u-aizu.ac.jp/onlinejudge/review.jsp?rid=6265747#2
    Thanks to https://prdc.hatenablog.com/entry/2017/09/12/140232

---
+++ dynamic_programming/DPL_1_C.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+# ref: https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=DPL_1_C&lang=ja
+
+n, w = gets.chomp.split.map(&:to_i)
+weights = Array.new(n)
+values = Array.new(n)
+
+n.times do |i|
+  values[i], weights[i] = gets.chomp.split.map(&:to_i)
+end
+
+# This satisfies the initial condition.
+dp = Array.new(n + 1) { Array.new(w + 1, 0) }
+
+n.times do |i|
+  (0..w).each do |j|
+    dp[i + 1][j] = if j >= weights[i]
+                     [(dp[i][j - weights[i]] + values[i]), (dp[i + 1][j - weights[i]] + values[i]), dp[i][j]].max
+                   else
+                     dp[i][j]
+                   end
+  end
+end
+
+puts dp[n][w]
