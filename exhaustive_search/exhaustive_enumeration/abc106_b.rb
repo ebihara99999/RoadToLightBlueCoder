5e110d32f6a82d4026925587b1bdd098ffdd4c34
ebihara99999 <dev.biibiebi@gmail.com>
2024-09-27 20:03:35 +0900

    Passed :tada:

--- exhaustive_search/exhaustive_enumeration/abc106_b.rb
+++ exhaustive_search/exhaustive_enumeration/abc106_b.rb
@@ -1,17 +1,15 @@
-# frozen_string_literal: true
-# ref: https://atcoder.jp/contests/abc106/tasks/abc106_b
 
 n = gets.chomp.to_i
+
+arr = (1..n).select { |i| i.odd? }
+
 result = 0
-
-(3..n).each do |num|
-  next if num.even?
-
-  divisors = [1, num]
-  (3..num - 1).each do |divisor|
-    divisors << divisor if (num % divisor).zero?
+(105..n).each do |x|
+  count = 0
+  arr.each do |y|
+    count += 1 if (x % y).zero?
   end
-  result += 1 if divisors.size == 8
+  result += 1 if count == 8
 end
 
-puts result
+p result
