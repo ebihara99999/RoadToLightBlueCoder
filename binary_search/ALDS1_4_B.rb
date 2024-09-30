fa93dc83ff161f43ba607ad0f86c1f86e5ed267a
ebihara99999 <dev.biibiebi@gmail.com>
2024-09-30 14:33:49 +0900

    Passed again!

--- binary_search/ALDS1_4_B.rb
+++ binary_search/ALDS1_4_B.rb
@@ -1,29 +1,24 @@
-# frozen_string_literal: true
 # ref: https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=ALDS1_4_B&lang=ja#
 
-_ = gets.chomp.to_i
-slist = gets.split.map(&:to_i)
-_ = gets.chomp.to_i
-tlist = gets.split.map(&:to_i)
+n = gets.chomp.to_i
+a = gets.chomp.split.map(&:to_i)
+_ = gets.chomp
+m = gets.chomp.split.map(&:to_i)
 
-cnt = 0
+results = []
+sums = []
 
-tlist.each do |t|
-  last = slist.size - 1
-  start = 0
-  loop do
-    idx = (start + (last - start) / 2).round
-    if slist[idx] < t
-      start = idx + 1
-      break if start == slist.size - 1
-    elsif slist[idx] == t
-      cnt += 1
-      break
-    else
-      last = idx - 1
-      break if last.zero?
-    end
+(1<<n).times do |i|
+  sum = 0
+
+  n.times do |bit|
+    sum += a[bit] if i & (1 << bit) != 0
   end
+  sums.push(sum)
 end
 
-p cnt
+m.each do |mi|
+  results.push(sums.include?(mi) ? 'yes' : 'no')
+end
+
+puts results
