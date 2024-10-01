1dd57323ab88fa207fb67326f661c065119941f4
ebihara99999 <dev.biibiebi@gmail.com>
2024-10-01 11:52:41 +0900

    Passed again!

--- exhaustive_search/exhaustive_enumeration/abc145_c.rb
+++ exhaustive_search/exhaustive_enumeration/abc145_c.rb
@@ -1,23 +1,21 @@
-# frozen_string_literal: true
 # ref: https://atcoder.jp/contests/abc145/tasks/abc145_c
 
 n = gets.chomp.to_i
-city = Array.new(n) { Array.new(2) }
+factorial = (1..n).inject(:*)
+
+coordinates = []
+
 n.times do |i|
-  city[i][0], city[i][1] = gets.split.map(&:to_i)
+  coordinates << gets.chomp.split.map(&:to_i)
 end
 
 distances = []
-routes = (0..n - 1).to_a.permutation.to_a
 
-routes.each do |route|
-  tmp = []
-  route.each_index do |i|
-    next if i == n - 1
-
-    tmp << Math.sqrt((city[route[i]][0] - city[route[i + 1]][0])**2 + (city[route[i]][1] - city[route[i + 1]][1])**2)
+(1..n).to_a.permutation(n).each do |cities|
+  cities.each_cons(2) do |city1, city2|
+    distances <<
+      Math.sqrt((coordinates[city1 - 1][0] - coordinates[city2 - 1][0])**2 + (coordinates[city1 - 1][1] - coordinates[city2 - 1][1])**2)
   end
-  distances << tmp.sum
 end
 
-p distances.sum / routes.size
+p distances.sum / factorial
