6e8fe5d77967bcedf2dc169a25b5a8a03e71e500
ebihara99999 <dev.biibiebi@gmail.com>
2024-10-01 17:40:09 +0900

    Edited a wrong file.

--- exhaustive_search/exhaustive_enumeration/ALDS1_5_A.rb
+++ exhaustive_search/exhaustive_enumeration/ALDS1_5_A.rb
@@ -1,22 +1,24 @@
 # ref: https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=ALDS1_5_A&lang=ja
 
 n = gets.chomp.to_i
+a = gets.chomp.split.map(&:to_i)
+_ = gets.chomp
+m = gets.chomp.split.map(&:to_i)
 
-list_a = gets.chomp.split(' ').map(&:to_i)
+results = []
+sums = []
 
-m = gets.chomp.to_i
-list_m = gets.chomp.split(' ').map(&:to_i)
+(1<<n).times do |i|
+  sum = 0
 
-list_sum = []
-1.upto(n) do |i|
-  list_sum << list_a.combination(i).to_a.map(&:sum)
-end
-list_sum.flatten!.uniq!
-
-list_m.each do |m|
-  if list_sum.include? m
-    puts 'yes'
-  else
-    puts 'no'
+  n.times do |bit|
+    sum += a[bit] if i & (1 << bit) != 0
   end
+  sums.push(sum)
 end
+
+m.each do |mi|
+  results.push(sums.include?(mi) ? 'yes' : 'no')
+end
+
+puts results
