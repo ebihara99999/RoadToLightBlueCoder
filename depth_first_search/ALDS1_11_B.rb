904730857abbb3331d24c901eb6eb250ed7a6922
ebihara99999 <dev.biibiebi@gmail.com>
2024-10-03 11:44:10 +0900

    Passed again!

--- depth_first_search/ALDS1_11_B.rb
+++ depth_first_search/ALDS1_11_B.rb
@@ -1,37 +1,35 @@
-# frozen_string_literal: true
+# ref: https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=ALDS1_11_B&lang=ja#
 
-$count = 0
-
+$counter = 0
 first_orders = []
 last_orders = []
 
-def dfs(graph, id, visited, first_orders, last_orders)
-  first_orders[id] = $count = $count += 1
-  visited[id] = true
+def dfs(graph, v, visited, first_orders, last_orders)
+  first_orders[v] = $counter　= $counter + 1
+  visited[v] = true
 
-  graph[id].each do |next_id|
-    next if visited[next_id]
-
-    dfs(graph, next_id, visited, first_orders, last_orders)
+  graph[v].each do |adj|
+    next if visited[adj]
+    dfs(graph, adj, visited, first_orders, last_orders)
   end
-  last_orders[id] = $count = $count += 1
+
+  last_orders[v] = $counter = $counter + 1
 end
 
 n = gets.chomp.to_i
+graph = Array.new(n) { []}
+visited = Array.new(n, false)
 
-graph = Array.new(n) { [] }
-
-n.times do
-  val, _, *adj = gets.chomp.split.map(&:to_i)
+n.times do |i|
+  _, _, *adj = gets.chomp.split.map(&:to_i)
   adj.each do |a|
-    graph[val - 1] << a - 1
+    graph[i] << a - 1
   end
 end
 
-visited = Array.new(n, false)
-while visited.any?(&:!)
-  id = visited.find_index(false)
-  dfs(graph, id, visited, first_orders, last_orders)
+while visited(&:!) do
+  v = visited.find_index(false)
+  dfs(graph, v, visited, first_orders, last_orders)
 end
 
 n.times do |i|
