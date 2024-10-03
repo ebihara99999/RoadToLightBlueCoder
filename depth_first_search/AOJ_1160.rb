38f7656ec8f4494efad03bcd7cbfb431bc7b75d1
ebihara99999 <dev.biibiebi@gmail.com>
2024-10-03 14:55:08 +0900

    Passed again!

--- depth_first_search/AOJ_1160.rb
+++ depth_first_search/AOJ_1160.rb
@@ -1,39 +1,40 @@
-# frozen_string_literal: true
 # ref: https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=1160&lang=jp
 
 @dx = [1, 1, 1, 0, 0, -1, -1, -1]
-@dy = [-1, 0, 1, -1, 1, -1, 0, 1]
+@dy = [1, 0, -1, 1, -1, 1, 0, -1]
 
 def dfs(h, w)
   @fields[h][w] = 0
   8.times do |i|
-    hh = h + @dy[i]
-    ww = w + @dx[i]
+    nh = h + @dx[i]
+    nw = w + @dy[i]
 
-    next if hh.negative? || ww.negative? || hh > @h - 1 || ww > @w - 1
+    next if nh.negative? || nh >= @h || nw.negative? || nw >= @w
+    next if @fields[nh][nw].zero?
 
-    next if @fields[hh][ww].zero?
-
-    dfs(hh, ww)
+    dfs(nh, nw)
   end
 end
 
 counts = []
+
 while (@w, @h = gets.chomp.split.map(&:to_i)) != [0, 0]
-  @fields = Array.new(@h) { Array.new(@w) { 0 } }
+  @fields = Array.new(@h) { Array.new(@w) }
   @h.times do |i|
     @fields[i] = gets.chomp.split.map(&:to_i)
   end
-  islands = 0
-  @h.times do |y|
-    @w.times do |x|
-      next if @fields[y][x].zero?
 
-      dfs(y, x)
-      islands += 1
+  count = 0
+
+  @h.times do |i|
+    @w.times do |j|
+      next if @fields[i][j].zero?
+
+      dfs(i, j)
+      count += 1
     end
   end
-  counts << islands
+  counts << count
 end
 
-counts.each { |c| p c }
+puts counts.join("\n")
