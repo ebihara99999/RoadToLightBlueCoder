e4bf98e472cb23095ec419f56a93083e7743cf72
ebihara99999 <dev.biibiebi@gmail.com>
2024-10-07 12:27:29 +0900

    Passed again.

--- depth_first_search/ALDS1_11_B.rb
+++ depth_first_search/ALDS1_11_B.rb
@@ -1,37 +1,36 @@
 # ref: https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=ALDS1_11_B&lang=ja#
+n = gets.to_i
 
-$counter = 0
-first_orders = []
-last_orders = []
-
-def dfs(graph, v, visited, first_orders, last_orders)
-  first_orders[v] = $counter　= $counter + 1
-  visited[v] = true
-
-  graph[v].each do |adj|
-    next if visited[adj]
-    dfs(graph, adj, visited, first_orders, last_orders)
-  end
-
-  last_orders[v] = $counter = $counter + 1
-end
-
-n = gets.chomp.to_i
-graph = Array.new(n) { []}
-visited = Array.new(n, false)
+graph = Array.new(n) { [] }
 
 n.times do |i|
-  _, _, *adj = gets.chomp.split.map(&:to_i)
-  adj.each do |a|
-    graph[i] << a - 1
-  end
+  graph[i] = gets.chomp.split.map { |x| x.to_i - 1 }
+  graph[i].shift
+  graph[i].shift
 end
 
-while visited(&:!) do
-  v = visited.find_index(false)
-  dfs(graph, v, visited, first_orders, last_orders)
+@first_orders = []
+@last_orders = []
+@order = 0
+seen = Array.new(n, false)
+
+def dfs(graph, v, seen)
+  seen[v] = true
+  @first_orders[v] = @order += 1
+
+  graph[v].each do |nv|
+    next if seen[nv]
+    dfs(graph, nv, seen)
+  end
+
+  @last_orders[v] = @order += 1
+end
+
+while seen.include?(false)
+  v = seen.index(false)
+  dfs(graph, v, seen)
 end
 
 n.times do |i|
-  puts "#{i + 1} #{first_orders[i]} #{last_orders[i]}"
+  puts "#{i + 1} #{@first_orders[i]} #{@last_orders[i]}"
 end
