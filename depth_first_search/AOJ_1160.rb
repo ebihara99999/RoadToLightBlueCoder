28333889b002d33e9b1a6aa9881267bd8f0d8cf0
ebihara99999 <dev.biibiebi@gmail.com>
2024-10-07 19:54:24 +0900

    Passed again!

--- depth_first_search/AOJ_1160.rb
+++ depth_first_search/AOJ_1160.rb
@@ -1,40 +1,40 @@
 # ref: https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=1160&lang=jp
+# 12:34
 
-@dx = [1, 1, 1, 0, 0, -1, -1, -1]
-@dy = [1, 0, -1, 1, -1, 1, 0, -1]
+@dw = [1, 1, 1, 0, 0, -1, -1, -1]
+@dh = [1, 0, -1, 1, -1, 1, 0, -1]
+
+@counts = []
 
 def dfs(h, w)
-  @fields[h][w] = 0
   8.times do |i|
-    nh = h + @dx[i]
-    nw = w + @dy[i]
+    nh = h + @dh[i]
+    nw = w + @dw[i]
 
-    next if nh.negative? || nh >= @h || nw.negative? || nw >= @w
-    next if @fields[nh][nw].zero?
-
+    next if nw.negative? || nh.negative? || nh >= @h || nw >= @w
+    next if @field[nh][nw].zero?
+    @field[nh][nw] = 0
     dfs(nh, nw)
   end
 end
 
-counts = []
+while (@w, @h = gets.chomp.split.map(&:to_i)) != [0, 0] do
+  @count = 0
 
-while (@w, @h = gets.chomp.split.map(&:to_i)) != [0, 0]
-  @fields = Array.new(@h) { Array.new(@w) }
+  @field = []
   @h.times do |i|
-    @fields[i] = gets.chomp.split.map(&:to_i)
+    @field[i] = gets.chomp.split.map(&:to_i)
   end
 
-  count = 0
-
-  @h.times do |i|
-    @w.times do |j|
-      next if @fields[i][j].zero?
-
-      dfs(i, j)
-      count += 1
+  @w.times do |wi|
+    @h.times do |hi|
+      next if @field[hi][wi].zero?
+      dfs(hi, wi)
+      @count += 1
     end
   end
-  counts << count
+
+  @counts << @count
 end
 
-puts counts.join("\n")
+puts @counts
