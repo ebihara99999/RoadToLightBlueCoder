b9c0a75c8fb9b88dad18952930d5ae6d36781731
ebihara99999 <dev.biibiebi@gmail.com>
2024-10-09 18:24:35 +0900

    Passed again!

--- breadth_first_search/ALDS1_11_C.rb
+++ breadth_first_search/ALDS1_11_C.rb
@@ -1,34 +1,28 @@
-# frozen_string_literal: true
-
 # ref: https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=ALDS1_11_C&lang=ja
 
-n = gets.to_i
+n = gets.chomp.to_i
 
-graph = Array.new(n) { [] }
-
+graph = Array.new(n)
 n.times do |i|
-  graph[i] = gets.chomp.split.map(&:to_i)
+  graph[i] = gets.chomp.split.map { |j| j.to_i - 1 }
   graph[i].shift
   graph[i].shift
 end
 
-queue = []
+
 dist = Array.new(n, -1)
+queue = Array.new
 
 dist[0] = 0
-queue << 0
+queue.push 0
 
 while !queue.empty?
   v = queue.shift
   graph[v].each do |nv|
-    nv -= 1
     next if dist[nv] != -1
-
     dist[nv] = dist[v] + 1
-    queue << nv
+    queue.push nv
   end
 end
 
-dist.each_with_index do |d, i|
-  puts "#{i + 1} #{d}"
-end
+dist.each_with_index { |d, i| puts "#{i + 1} #{d}\n" }
