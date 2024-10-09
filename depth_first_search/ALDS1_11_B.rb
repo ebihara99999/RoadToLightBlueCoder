a3645c9e524dda8aaffdc42399f2fbdb23002d9e
ebihara99999 <dev.biibiebi@gmail.com>
2024-10-09 16:42:19 +0900

    Passed again.

--- depth_first_search/ALDS1_11_B.rb
+++ depth_first_search/ALDS1_11_B.rb
@@ -1,36 +1,34 @@
 # ref: https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=ALDS1_11_B&lang=ja#
-n = gets.to_i
 
-graph = Array.new(n) { [] }
+n = gets.chomp.to_i
 
+graph = []
 n.times do |i|
-  graph[i] = gets.chomp.split.map { |x| x.to_i - 1 }
+  graph[i] = gets.chomp.split.map { |v| v.to_i - 1 }
   graph[i].shift
   graph[i].shift
 end
 
+seen = Array.new(n, false)
+
 @first_orders = []
 @last_orders = []
-@order = 0
-seen = Array.new(n, false)
+@timestamp = 0
 
 def dfs(graph, v, seen)
   seen[v] = true
-  @first_orders[v] = @order += 1
-
+  @first_orders[v] = @timestamp += 1
   graph[v].each do |nv|
     next if seen[nv]
     dfs(graph, nv, seen)
   end
 
-  @last_orders[v] = @order += 1
+  @last_orders[v] = @timestamp += 1
 end
 
 while seen.include?(false)
-  v = seen.index(false)
-  dfs(graph, v, seen)
+  start = seen.index(false)
+  dfs(graph, start, seen)
 end
 
-n.times do |i|
-  puts "#{i + 1} #{@first_orders[i]} #{@last_orders[i]}"
-end
+puts n.times.map { |i| "#{i + 1} #{@first_orders[i]} #{@last_orders[i]}" }.join("\n")
