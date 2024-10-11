c8009f022a5f2df9ed840d93ab6616ab68426d8f
ebihara99999 <dev.biibiebi@gmail.com>
2024-10-11 18:37:24 +0900

    Passed again!

--- dynamic_programming/ALDS1_10_A.rb
+++ dynamic_programming/ALDS1_10_A.rb
@@ -1,17 +1,22 @@
 # frozen_string_literal: true
+
 # ref: https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=ALDS1_10_A&lang=ja
 
-def fib(n)
-  list = Array.new(n, 0)
-  list[0] = 1
-  list[1] = 1
-  return list[n] if n < 2
+@memo = []
 
-  (2..n).each do |i|
-    list[i] = list[i - 1] + list[i - 2]
+def fib(n)
+  if n.zero?
+    1
+  elsif n == 1
+    1
+  else
+    if @memo[n].nil?
+      @memo[n] = fib(n - 1) + fib(n - 2)
+    else
+      @memo[n]
+    end
   end
-  list[n]
 end
 
-n = gets.to_i
-p fib(n)
+n = gets.chomp.to_i
+puts fib(n)
