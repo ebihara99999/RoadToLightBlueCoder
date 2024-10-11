b29295a883b5d7fa2c438a2aac1a3139d32e4c52
ebihara99999 <dev.biibiebi@gmail.com>
2024-10-11 19:54:54 +0900

    Passed again!

--- dynamic_programming/DPL_1_B.rb
+++ dynamic_programming/DPL_1_B.rb
@@ -2,25 +2,28 @@
 
 # ref: https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=DPL_1_B&lang=ja
 
-n, w = gets.chomp.split.map(&:to_i)
-weights = Array.new(n)
-values = Array.new(n)
+@n, @w = gets.chomp.split.map(&:to_i)
 
-n.times do |i|
-  values[i], weights[i] = gets.chomp.split.map(&:to_i)
+values = []
+weights = []
+
+@n.times do
+  v, w = gets.chomp.split.map(&:to_i)
+  values << v
+  weights << w
 end
 
-# This satisfies the initial condition.
-dp = Array.new(n + 1) { Array.new(w + 1, 0) }
-
-n.times do |i|
-  (0..w).each do |j|
-    dp[i + 1][j] = if j >= weights[i]
-                     [(dp[i][j - weights[i]] + values[i]), dp[i][j]].max
-                   else
-                     dp[i][j]
-                   end
+dp = Array.new(@n + 1) { Array.new(@w + 1, 0) }
+@max = 0
+@n.times do |i|
+  (@w + 1).times do |w|
+    if weights[i] <= w
+      dp[i + 1][w] = [dp[i][w], (dp[i][w - weights[i]] + values[i])].max
+      @max = [@max, dp[i + 1][w]].max
+    else
+      dp[i + 1][w] = dp[i][w]
+    end
   end
 end
 
-puts dp[n][w]
+puts @max
