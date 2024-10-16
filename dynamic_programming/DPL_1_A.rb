0395ff41edb7c0691e057a614ea6c98da286430d
ebihara99999 <dev.biibiebi@gmail.com>
2024-10-16 18:52:16 +0900

    Passed again!

--- dynamic_programming/DPL_1_A.rb
+++ dynamic_programming/DPL_1_A.rb
@@ -3,7 +3,6 @@
 # ref: https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=DPL_1_A&lang=ja
 
 n, m = gets.chomp.split.map(&:to_i)
-
 c = gets.chomp.split.map(&:to_i)
 
 dp = Array.new(n + 1, 100_000)
@@ -11,8 +10,8 @@ dp[0] = 0
 
 (n + 1).times do |i|
   m.times do |j|
-    dp[i + c[j]] = [dp[i + c[j]], dp[i] + 1].min if i + c[j] <= n
+    dp[i + c[j]] = [dp[i + c[j]], dp[i] + 1].min if c[j] + i <= n
   end
 end
 
-puts dp[n]
+p dp[n]
--- dynamic_programming/DPL_1_C.rb
+++ dynamic_programming/DPL_1_C.rb
@@ -2,25 +2,19 @@
 
 # ref: https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=DPL_1_C&lang=ja
 
+n, @w = gets.chomp.split.map(&:to_i)
+weights = Array.new(n)
+values = Array.new(n)
 
-@n, @w = gets.chomp.split.map(&:to_i)
-values = Array.new(@n)
-weights = Array.new(@n)
-
-@n.times do |i|
+n.times do |i|
   values[i], weights[i] = gets.chomp.split.map(&:to_i)
 end
 
 dp = Array.new(@w + 1, 0)
-dp[0] = 0
 
 (@w + 1).times do |w|
-  @n.times do |i|
-    dp[w] = if w >= weights[i]
-              [dp[w - weights[i]] + values[i], dp[w]].max
-            else
-              dp[w]
-            end
+  n.times do |i|
+    dp[w] = [dp[w], dp[w - weights[i]] + values[i]].max if w >= weights[i]
   end
 end
 
