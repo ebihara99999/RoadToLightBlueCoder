54aaa0fe3d7e10f8e791a5b75b452e717d73475c
ebihara99999 <dev.biibiebi@gmail.com>
2024-10-16 17:37:52 +0900

    Passed again!

--- dynamic_programming/DPL_1_A.rb
+++ dynamic_programming/DPL_1_A.rb
@@ -3,20 +3,16 @@
 # ref: https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=DPL_1_A&lang=ja
 
 n, m = gets.chomp.split.map(&:to_i)
-coins = gets.chomp.split.map(&:to_i)
-coins.unshift 0
 
-dp = Array.new(m + 1) { Array.new(n + 1) }
-(0..n).each { |jj| dp[0][jj] = n + 100 }
-(0..m).each { |ii| dp[ii][0] = 0 }
-(1..m).each do |i|
-  (1..n).each do |j|
-    dp[i][j] = if j < coins[i]
-                 dp[i - 1][j]
-               else
-                 [dp[i - 1][j], 1 + dp[i][j - coins[i]]].min
-               end
+c = gets.chomp.split.map(&:to_i)
+
+dp = Array.new(n + 1, 100_000)
+dp[0] = 0
+
+(n + 1).times do |i|
+  m.times do |j|
+    dp[i + c[j]] = [dp[i + c[j]], dp[i] + 1].min if i + c[j] <= n
   end
 end
 
-puts dp.transpose[n].min
+puts dp[n]
